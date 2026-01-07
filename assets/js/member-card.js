10696d87ae0c85f6eee49c41a65e24fdba194926
KhmtDan <khikhmatdan@gmail.com>
2026-01-07 17:17:23 +0300

    Add files via upload

---
+++ assets/js/member-card.js
@@ -0,0 +1,7 @@
+const fuckMessage = document.querySelectorAll(".ban-button");
+
+fuckMessage.forEach((button) => {
+  button.addEventListener("click", function () {
+    alert("Сосни хуйца");
+  });
+});
