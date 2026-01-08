0dcd9802cf5a2a2267af5987186646763deb178e
KhmtDan <khikhmatdan@gmail.com>
2026-01-08 10:57:58 +0300

    Add files via upload

---
+++ assets/js/steam-view.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const listElement = document.getElementById("steam-game-list");
+  fetch("./data/steam_data.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Файл данных пока не создан");
+      }
+      return response.json();
+    })
+    .then((games) => {
+      if (games.length === 0) {
+        listElement.innerHTML =
+          '<li class="loading-text">Данные скрыты настройками приватности Steam</li>';
+        return;
+      }
+      listElement.innerHTML = "";
+      games.forEach((game) => {
+        const li = document.createElement("li");
+        li.className = "game-card";
+        li.innerHTML = `img src="${game.image} alt="${game.name}" class="game-img" <div class="game-info">
+                        <span class="game-title">${game.name}</span>
+                        <span class="game-hours">⏱ ${game.hours} ч.</span>
+                    </div>`;
+        listElement.appendChild(li);
+      });
+    })
+    .catch((error) => {
+      console.error("Ошибка:", error);
+      listElement.innerHTML =
+        '<li class="loading-text">Данные обновляются... Зайдите позже.</li>';
+    });
+});
